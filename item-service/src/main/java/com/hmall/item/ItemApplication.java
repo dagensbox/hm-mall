0353b2121024e97ce645c3ee8942f112fe41ac0e
box <1214142612@qq.com>
2022-08-26 16:30:32 +0800

    商品服务CRUD 优化跨域访问

--- item-service/src/main/java/com/hmall/item/ItemApplication.java
+++ item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -1,13 +1,24 @@
 package com.hmall.item;
 
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.bouncycastle.pqc.crypto.newhope.NHOtherInfoGenerator;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @MapperScan("com.hmall.item.mapper")
 @SpringBootApplication
 public class ItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemApplication.class, args);
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
     }
 }
--- item-service/src/main/java/com/hmall/item/web/ItemController.java
+++ item-service/src/main/java/com/hmall/item/web/ItemController.java
@@ -1,14 +1,83 @@
 package com.hmall.item.web;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.common.dto.PageDTO;
+import com.hmall.item.pojo.Item;
 import com.hmall.item.service.IItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("item")
+@RequestMapping("/item")
 public class ItemController {
 
     @Autowired
     private IItemService itemService;
+
+    /**
+     * 分页查询商品
+     *
+     * @param page 当前所在页
+     * @param size 页码大小
+     * @return 分页返回PageDTO
+     */
+    @GetMapping("/list")
+    public PageDTO<Item> list(int page, int size) {
+        Page<Item> page1 = new Page<>(page, size);
+        itemService.page(page1);
+        PageDTO<Item> pageDTO = new PageDTO<>();
+        pageDTO.setTotal(page1.getTotal());
+        pageDTO.setList(page1.getRecords());
+        return pageDTO;
+    }
+
+    /**
+     * 根据id查询商品
+     *
+     * @param id 商品id
+     * @return 商品对象
+     */
+    @GetMapping("/{id}")
+    public Item getItemById(@PathVariable Long id) {
+        return itemService.getById(id);
+    }
+
+    /**
+     * 新增商品
+     *
+     * @param item 商品
+     */
+    @PostMapping
+    public void addItem(@RequestBody Item item) {
+        itemService.save(item);
+    }
+
+    /**
+     * 上架下架商品
+     *
+     * @param id     商品id
+     * @param status 修改后的状态，上架1，下架2
+     */
+    @PutMapping("/status/{id}/{status}")
+    public void updateItemStatusById(@PathVariable Long id, @PathVariable Integer status) {
+        Item item = new Item();
+        item.setId(id);
+        item.setStatus(status);
+        itemService.updateById(item);
+    }
+
+    /**
+     * 修改商品
+     *
+     * @param item 修改的商品信息
+     */
+    @PutMapping
+    public void updateItemById(@RequestBody Item item) {
+        itemService.updateById(item);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteItemById(@PathVariable Long id){
+        itemService.removeById(id);
+    }
 }
