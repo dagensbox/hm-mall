343759535d63002573f294a3b1796c32bcd8ab14
box <1214142612@qq.com>
2022-08-27 14:49:17 +0800

    登录信息完成

---
+++ user-service/src/main/java/com/hmall/user/interceptor/MyHeadInterceptor.java
@@ -0,0 +1,30 @@
+package com.hmall.user.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author 12141
+ */
+@Component
+public class MyHeadInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String userId = request.getHeader("authorization");
+        if (StringUtils.isEmpty(userId)) {
+            return false;
+        }
+        ThreadLocalUtils.setCurrentUserId(Long.valueOf(userId));
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        ThreadLocalUtils.remove();
+    }
+}
---
+++ user-service/src/main/java/com/hmall/user/interceptor/ThreadLocalUtils.java
@@ -0,0 +1,21 @@
+package com.hmall.user.interceptor;
+
+/**
+ * @author 12141
+ */
+public class ThreadLocalUtils {
+
+    private static final ThreadLocal<Long> THREAD_LOCAL = new ThreadLocal<>();
+
+    public static void setCurrentUserId(Long userId) {
+        THREAD_LOCAL.set(userId);
+    }
+
+    public static Long getCurrentUserId() {
+        return THREAD_LOCAL.get();
+    }
+
+    public static void remove() {
+        THREAD_LOCAL.remove();
+    }
+}
